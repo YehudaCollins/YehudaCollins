6c7412c71c4aa2ac4912b7b924bec832c1928da8
YehudaCollins <ycollins555@ email.com>
2024-07-19 06:40:01 +0300

    ץ

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Style/index1.css';
+import './Style/index1.css'; // וודא שהנתיב נכון
+
 import App from './App';
 
 ReactDOM.render(
