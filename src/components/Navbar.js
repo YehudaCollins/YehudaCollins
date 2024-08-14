98574e336d3541d9d4c256bb2944ae94bf118cb3
YehudaCollins <ycollins555@ email.com>
2024-08-15 02:07:22 +0300

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import { FaHome, FaProjectDiagram, FaUser, FaEnvelope } from 'react-icons/fa';
-import { gsap } from 'gsap';
+import React, { useState } from 'react';
+import { FaBars, FaTimes, FaHome, FaProjectDiagram, FaUser, FaEnvelope } from 'react-icons/fa';
 import { Link } from 'react-scroll';
-import '../Style/Navbar.css'; // ייבוא קובץ ה-CSS
+import '../Style/Navbar.css';
 
 function Navbar() {
-  React.useEffect(() => {
-    gsap.from('.nav-link', { duration: 1, opacity: 0, y: -50, stagger: 0.3 });
-  }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="navbar">
-      <div className="navbar-title">Full stack</div>
-      <Link to="home" smooth={true} duration={1000} className="nav-link">
-        <FaHome className="nav-icon" /> About
-      </Link>
-      <Link to="skills" smooth={true} duration={1000} className="nav-link">
-        <FaProjectDiagram className="nav-icon" /> Skills
-      </Link>
-      <Link to="work" smooth={true} duration={1000} className="nav-link">
-        <FaUser className="nav-icon" /> Work
-      </Link>
-      <Link to="contact" smooth={true} duration={1000} className="nav-link">
-        <FaEnvelope className="nav-icon" /> Contact
-      </Link>
+      <div className="navbar-title">YC</div>
+
+      <div className={`nav-links-container ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="home" smooth={true} duration={1000} className="nav-link" onClick={toggleMenu}>
+          <FaHome className="nav-icon" /> Home
+        </Link>
+        <Link to="skills" smooth={true} duration={1000} className="nav-link" onClick={toggleMenu}>
+          <FaProjectDiagram className="nav-icon" /> Skills
+        </Link>
+        <Link to="work" smooth={true} duration={1000} className="nav-link" onClick={toggleMenu}>
+          <FaUser className="nav-icon" /> Work
+        </Link>
+        <Link to="contact" smooth={true} duration={1000} className="nav-link" onClick={toggleMenu}>
+          <FaEnvelope className="nav-icon" /> Contact
+        </Link>
+      </div>
+
+      <div className={`hamburger-icon ${isMenuOpen ? 'open' : 'close'}`} onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </div>
     </nav>
   );
 }
